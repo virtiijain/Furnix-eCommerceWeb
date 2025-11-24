0cdf41fd23fd6eea82ef92fbf57165686b644286
Virti Jain <virtij29@gmail.com>
2025-11-24 14:05:24 +0530

    ci test

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
+  "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:4173",
   "https://e-commerce-webb.vercel.app",
